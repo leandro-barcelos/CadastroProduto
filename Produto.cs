34cfdf0333f79cecc1f4378d59599d6940af494a
leand <leand@DESKTOP-GAMES>
2023-09-18 21:01:06 -0300

    Add project files.

---
+++ Produto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroProduto
+{
+    public class Produto
+    {
+        public int Id { get; private set; }
+
+        public string Nome { get; private set; }
+
+        public double Preco { get; private set; }
+
+        public Produto(string nome, double preco) 
+        { 
+            this.Nome = nome;
+            this.Preco = preco;
+        }
+
+        public class ProdutoRepository
+        {
+            private List<Produto> produtos = new();
+            private int proxId = 1;
+
+            public void Create(Produto produto)
+            {
+                produto.Id = proxId;
+                proxId++;
+                produtos.Add(produto);
+            }
+
+            public void Delete(int id)
+            {
+                Produto produtoDeletar = ReadById(id);
+                if (produtoDeletar != null)
+                {
+                    produtos.Remove(produtoDeletar);
+                }
+            }
+
+            public List<Produto> ListAll()
+            {
+                return produtos;
+            }
+
+            public Produto ReadById(int id)
+            {
+                return produtos.Find(produto => produto.Id == id);
+            }
+
+            public void Update(int id, string novoNome)
+            {
+                Produto produto = ReadById(id);
+                if (produto != null)
+                {
+                    produto.Nome = novoNome;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using CadastroProduto;
+using System;
+
+Produto.ProdutoRepository produtoRepository = new();
+
+bool sair = false;
+
+while (!sair)
+{
+    Console.Clear();
+    Console.WriteLine("Menu Principal:");
+    Console.WriteLine("1. Adicionar um novo item");
+    Console.WriteLine("2. Remover item pelo ID");
+    Console.WriteLine("3. Editar um item pelo ID ");
+    Console.WriteLine("4. Ver todos os itens cadastrados");
+    Console.WriteLine("5. Sair");
+    Console.Write("Escolha uma opção: ");
+
+    string opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            AdicionarNovoProduto();
+            break;
+        case "2":
+            RemoverProduto();
+            break;
+        case "3":
+            EditarProduto();
+            break;
+        case "4":
+            ListarProdutos();
+            break;
+        case "5":
+            sair = true;
+            break;
+        default:
+            Console.WriteLine("Opção inválida. Pressione Enter para continuar.");
+            Console.ReadLine();
+            break;
+    }
+}
+
+void AdicionarNovoProduto()
+{
+    Console.Clear();
+    Console.WriteLine("Adicionar um novo produto:");
+
+    Console.Write("Nome do produto: ");
+    string nome = Console.ReadLine();
+
+    Console.Write("Preço do produto: ");
+    double preco;
+    if (double.TryParse(Console.ReadLine(), out preco))
+    {
+        Produto produto = new(nome, preco);
+        produtoRepository.Create(produto);
+        Console.WriteLine("Produto adicionado com sucesso!");
+    }
+    else
+    {
+        Console.WriteLine("Preço inválido. O produto não foi adicionado.");
+    }
+
+    Console.WriteLine("Pressione Enter para continuar.");
+    Console.ReadLine();
+}
+
+void RemoverProduto()
+{
+    Console.Clear();
+    Console.WriteLine("Remover produto:");
+    Console.Write("Digite o ID do produto que deseja remover: ");
+
+    if (int.TryParse(Console.ReadLine(), out int id))
+    {
+        Produto produto = produtoRepository.ReadById(id);
+        if (produto != null)
+        {
+            produtoRepository.Delete(id);
+            Console.WriteLine("Produto removido com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Produto não encontrado.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("ID inválido.");
+    }
+
+    Console.WriteLine("Pressione Enter para continuar.");
+    Console.ReadLine();
+}
+
+void EditarProduto()
+{
+    Console.Clear();
+    Console.WriteLine("Editar um produto:");
+    Console.Write("Digite o ID do produto que deseja editar: ");
+
+    if (int.TryParse(Console.ReadLine(), out int id))
+    {
+        Produto produto = produtoRepository.ReadById(id);
+        if (produto != null)
+        {
+            Console.Write("Novo nome do produto: ");
+            string novoNome = Console.ReadLine();
+
+            produtoRepository.Update(id, novoNome);
+            Console.WriteLine("Produto atualizado com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Produto não encontrado.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("ID inválido.");
+    }
+
+    Console.WriteLine("Pressione Enter para continuar.");
+    Console.ReadLine();
+}
+
+void ListarProdutos()
+{
+    Console.Clear();
+    Console.WriteLine("Todos os produtos:");
+    List<Produto> produtos = produtoRepository.ListAll();
+
+    foreach (var produto in produtos)
+    {
+        Console.WriteLine($"ID: {produto.Id}, Nome: {produto.Nome}, Preço: {produto.Preco}");
+    }
+
+    Console.WriteLine("Pressione Enter para continuar.");
+    Console.ReadLine();
+}
